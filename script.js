65b5b342126ffba5283d1bbe5634eee534afbfc9
Karen Valentim <karenvalentim54@gmail.com>
2025-09-05 11:30:50 -0300

    Desing
    adição de placar, fundo de tela e botão de reiniciar

--- script.js
+++ script.js
@@ -1,38 +1,52 @@
 const polvo = document.querySelector('.polvo');
 const pipe = document.querySelector('.pipe');
+const restartBtn = document.getElementById('restart-btn');
+const scoreDisplay = document.getElementById('score');
+
+let score = 0;
+let isGameOver = false;
 
 const jump = () => {
-    polvo.classList.add('jump');
+    if (!isGameOver) {
+        polvo.classList.add('jump');
 
-
-    setTimeout(() => {
-        polvo.classList.remove('jump');
-    }, 500);
-
-
-}
-
+        setTimeout(() => {
+            polvo.classList.remove('jump');
+        }, 500);
+    }
+};
 
 const loop = setInterval(() => {
-
     const pipePosition = pipe.offsetLeft;
     const polvoPosition = +window.getComputedStyle(polvo).bottom.replace('px', '');
 
-    if (pipePosition <= 120 && pipePosition > 0 && polvoPosition < 80) {
 
+    if (pipePosition <= 120 && pipePosition > 0 && polvoPosition < 80) {
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
 
         polvo.style.animation = 'none';
-        polvo.style.left = `${polvoPosition}px`;
+        polvo.style.bottom = `${polvoPosition}px`;
 
         polvo.src = 'imagens/game-over.png';
         polvo.style.width = '75px';
         polvo.style.marginLeft = '50px';
 
         clearInterval(loop);
+        restartBtn.style.display = 'block';
+        isGameOver = true;
     }
 
+
+    if (pipePosition < 0 && !isGameOver) {
+        score++;
+        scoreDisplay.textContent = score;
+        pipe.style.right = '-100px';
+    }
 }, 10);
 
 document.addEventListener('keydown', jump);
+
+restartBtn.addEventListener('click', () => {
+    location.reload();
+});
